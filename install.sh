4cf6fb360f68c74f3c3f5bd03256846ebaac608c
tom <worldofth@hotmail.co.uk>
2022-04-09 13:04:42 +0100

    update install

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-if [[ -e ~/.zshrc.bak ]]; then
-elif [[ -e ~/.zshrc ]]; then
+if [[ ! -e ~/.zshrc.bak && -e ~/.zshrc ]]; then
     mv ~/.zshrc ~/.zshrc.bak
 fi
 
-stow --target=/home/tom/ ./zsh
+stow --target=/home/tom/ zsh
